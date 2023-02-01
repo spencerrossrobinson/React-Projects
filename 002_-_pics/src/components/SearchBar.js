abf4a09800bfc14ac8364451fdb66aa9793ab66f
Spencer Robinson <spencerwarmouth@gmail.com>
2023-02-01 18:26:49 -0500

    Child to Parent Event Passing

--- 002_-_pics/src/components/SearchBar.js
+++ 002_-_pics/src/components/SearchBar.js
@@ -1,5 +1,14 @@
-function SearchBar() {
-  return <div>SearchBar</div>;
-}
+const SearchBar = ({ onSubmit }) => {
+  const handleClick = () => {
+    onSubmit("cars");
+  };
+
+  return (
+    <div>
+      <input />
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
+};
 
 export default SearchBar;
