cc325bd35a65bffe297a94e575eff9edaff7ee2b
Spencer Robinson <spencerwarmouth@gmail.com>
2023-02-07 14:18:57 -0500

    Book Create Page

--- 002_-_books/src/App.js
+++ 002_-_books/src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div>App</div>;
-}
+import BookCreate from "./components/BookCreate";
+import { useState } from "react";
+
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  const handleCreateBook = (title) => {
+    console.log("Need to add book with this title", title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={handleCreateBook} />
+    </div>
+  );
+};
 
 export default App;
--- 002_-_books/src/components/BookCreate.js
+++ 002_-_books/src/components/BookCreate.js
@@ -1,5 +1,28 @@
-function BookCreate() {
-  return <div>BookCreate</div>;
-}
+import { useState } from "react";
+
+const BookCreate = ({ onCreate }) => {
+  const [title, setTitle] = useState("");
+
+  const onHandleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onHandleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle("");
+  };
+
+  return (
+    <div className="book-create">
+      <h3>Add a Book</h3>
+      <form onSubmit={onHandleSubmit}>
+        <label>Title</label>
+        <input className="input" value={title} onChange={onHandleChange} />
+        <button className="button">Create!</button>
+      </form>
+    </div>
+  );
+};
 
 export default BookCreate;
