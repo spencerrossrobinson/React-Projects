0f73f8557d0713fe09419bae4aa9223277542e72
Spencer Robinson <spencerwarmouth@gmail.com>
2023-02-09 13:34:08 -0500

    Book Edit Feature Completed

--- 002_-_books/src/components/BookEdit.js
+++ 002_-_books/src/components/BookEdit.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const BookEdit = ({ book }) => {
+const BookEdit = ({ book, onEdit }) => {
   const [title, setTitle] = useState(book.title);
   console.log(title);
 
@@ -9,7 +9,7 @@ const BookEdit = ({ book }) => {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("new title is ", title);
+    onEdit(book.id, title);
   };
 
   return (
--- 002_-_books/src/components/BookShow.js
+++ 002_-_books/src/components/BookShow.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import BookEdit from "./BookEdit";
 
-const BookShow = ({ book, onDelete }) => {
+const BookShow = ({ book, onDelete, onEdit }) => {
   const [showEdit, setShowEdit] = useState(false);
 
   const handleDeleteClick = () => {
@@ -13,7 +13,7 @@ const BookShow = ({ book, onDelete }) => {
 
   let content = <h3>{book.title}</h3>;
   if (showEdit) {
-    content = <BookEdit book={book} />;
+    content = <BookEdit book={book} onEdit={onEdit} />;
   }
 
   return (
