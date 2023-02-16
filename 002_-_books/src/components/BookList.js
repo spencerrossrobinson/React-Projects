66975fd45b2ce58a0f14b1fbdd1879737c226eba
Spencer Robinson <spencerwarmouth@gmail.com>
2023-02-16 12:00:42 -0500

    Groundwork for Context

--- 002_-_books/src/components/BookList.js
+++ 002_-_books/src/components/BookList.js
@@ -1,13 +1,22 @@
 import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/books";
 
 const BookList = ({ books, onDelete, onEdit }) => {
+  const value = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
     return (
       <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     );
   });
 
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      {value}
+      {renderedBooks}
+    </div>
+  );
 };
 
 export default BookList;
