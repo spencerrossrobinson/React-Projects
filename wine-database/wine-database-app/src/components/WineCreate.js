054df79e60bc29ddcf1a50b04377108336805010
Spencer Robinson <spencerwarmouth@gmail.com>
2023-02-25 21:30:58 -0500

    Adding a Bootstrap Form

--- wine-database/wine-database-app/src/components/WineCreate.js
+++ wine-database/wine-database-app/src/components/WineCreate.js
@@ -1,29 +1,42 @@
 import { useState } from "react";
 import useWinesContext from "../hooks/use-wines-context";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const WineCreate = () => {
+  //setting up the local state for the page that watches the Title of A Wine
   const [title, setTitle] = useState("");
+  // imports create wine function from Context
   const { handleCreateWine } = useWinesContext();
 
+  //function that watches the input for changes
   const onHandleChange = (event) => {
     setTitle(event.target.value);
   };
-
+  // function that will render the new title and photo when the form is submitted
   const onHandleSubmit = (event) => {
     event.preventDefault();
     handleCreateWine(title);
     setTitle("");
+    console.log(setTitle);
   };
 
   return (
+    //initial set up of the wine card that is built out with bootstrap
     <div className="">
       <h3>Add a Wine</h3>
-      <form onSubmit={onHandleSubmit}>
-        <label>Title</label>
-        <input className="input" value={title} onChange={onHandleChange} />
-        <Button variant="success"> Create!</Button>
-      </form>
+      <Form onSubmit={onHandleSubmit}>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          placehold="Enter a New Wine"
+          type="input"
+          value={title}
+          onChange={onHandleChange}
+        />
+        <Button variant="success" type="submit">
+          Create!
+        </Button>
+      </Form>
     </div>
   );
 };
